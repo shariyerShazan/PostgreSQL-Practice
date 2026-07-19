a7a875aa2850aa494482ed0d23a7d8469acd0af5
shariyerShazan <shariyershazan1@gmail.com>
2026-07-20 01:30:13 +0600

    first commit

---
+++ postgres/1_connection.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    age INT
+);
+
+INSERT INTO users (name, age)
+VALUES
+('Shariyer', 22),
+('Alice', 25);
+
+SELECT * FROM users;
